7c77445a2ca8d6abee5b02d097318affb9b74e69
John Valai <git@jvk.to>
2023-08-23 18:34:41 +0100

    bug: fix commit list

--- release-tool.sh
+++ release-tool.sh
@@ -14,15 +14,16 @@ get_changelog () {
         COMMITS_MSG
 
   # Get current tag
-  TAG_NEW=$(git describe --tags "$(git rev-list --tags --max-count=1)" )
+  TAG_NEW=$( git describe --tags "$( git rev-list --tags --max-count=1 )" )
   # Get the last commit before current tag
   LAST_TAG_COMMIT=$( git rev-list --tags --skip=1 --max-count=1 )
   # Get last tag
   TAG_PREV=$( git describe --abbrev=0 --tags "$LAST_TAG_COMMIT" )
   # Today's date
-  NOW="$(date +'%B %d, %Y')"
-  # Set range of commits between last tag and current head
-  RANGE=$( [ "$( git tag -l "${TAG_PREV}")" ] && echo "${TAG_PREV}...HEAD" )
+  NOW="$( date +'%B %d, %Y' )"
+  # Set range of commits between last tag and current tag
+  # - If no previous tag, range will be blank, which will show all commits
+  RANGE=$( [ "$( git tag -l "${TAG_PREV}" )" ] && echo "${TAG_PREV}...${TAG_NEW}" )
   COMMITS_MSG=$( git log --oneline --pretty=format:"- %h %s" "${RANGE}" 2>&1 )
   # shellcheck disable=SC2181
   if [ ! "$?" -eq 0 ]; then
